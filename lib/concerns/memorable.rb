f1c78075864f8e8ed5f8eb68645f6ff3ec0e1212
Farhan <farhanmahmoodny@gmail.com>
2019-02-05 13:42:21 -0500

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,18 @@
+
+module Memorable
+
+  module ClassMethod
+  def reset_all
+    self.all.clear
+  end
+  def count
+    self.all.count
+  end
+end
+
+  module InstanceMethod
+    def initialize
+      self.class.all << self
+    end
+  end
+end
